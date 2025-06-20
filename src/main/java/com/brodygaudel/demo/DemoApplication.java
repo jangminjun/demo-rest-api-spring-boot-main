530d89f02f1f04cf36d133c452724c08a3814257
jangminjun <chocolariver@naver.com>
2025-06-20 13:27:12 +0900

    eap pom.xml

--- src/main/java/com/brodygaudel/demo/DemoApplication.java
+++ src/main/java/com/brodygaudel/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.brodygaudel.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +11,9 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
+    
+    //@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 }
